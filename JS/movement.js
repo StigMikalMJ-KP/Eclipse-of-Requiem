34c0865cd3bdac6b5b5a602c4fee6ab569c7e1da
Lakimgits <johansenme@afk.no>
2026-04-09 11:23:35 +0200

    Temp fix for punishment logs

--- JS/movement.js
+++ JS/movement.js
@@ -1,11 +1,15 @@
 let player = document.getElementById("player");
+const map = document.getElementById("map");
 
 const SPEED = 9;
-let xPos = 100;
-let yPos = 100;
+let xPos = 500;
+let yPos = 500;
 
 player.style.left = xPos;
 player.style.top = yPos;
+
+let mapRect = map.getBoundingClientRect();
+
 
 document.addEventListener("keydown", (e) => {
     const LEFT = ["a", "ArrowLeft"];
